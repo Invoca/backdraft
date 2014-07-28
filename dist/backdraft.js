3f482bf2d2eb782e8b0e9d70bff9c3bda5775e50
Euge Gimelberg <egimelberg@invoca.com>
2014-07-28 12:40:10 -0700

    adding support for subclassing views, collections, routers, models

--- dist/backdraft.js
+++ dist/backdraft.js
@@ -404,23 +404,55 @@ _.extend(Plugin.factory, {
   // factories
   plugin.initializer(function(app) {
     app.Views = {};
-    app.view = function(name, properties) {
-      app.Views[name] = View.extend(properties);
+    app.view = function(name, baseClassName, properties) {
+      var baseClass;
+      if (arguments.length === 2) {
+        properties = baseClassName;
+        baseClass = View;
+      } else {
+        baseClass = app.Views[baseClassName];
+      }
+
+      app.Views[name] = baseClass.extend(properties);
     };
 
     app.Collections = {}
-    app.collection = function(name, properties) {
-      app.Collections[name] = Collection.extend(properties);
+    app.collection = function(name, baseClassName, properties) {
+      var baseClass;
+      if (arguments.length === 2) {
+        properties = baseClassName;
+        baseClass = Collection;
+      } else {
+        baseClass = app.Collections[baseClassName];
+      }
+
+      app.Collections[name] = baseClass.extend(properties);
     };
 
     app.Models = {};
-    app.model = function(name, properties) {
-      app.Models[name] = Model.extend(properties);
+    app.model = function(name, baseClassName, properties) {
+      var baseClass;
+      if (arguments.length === 2) {
+        properties = baseClassName;
+        baseClass = Model;
+      } else {
+        baseClass = app.Models[baseClassName];
+      }
+
+      app.Models[name] = baseClass.extend(properties);
     };
 
     app.Routers = {};
-    app.router = function(name, properties) {
-      app.Routers[name] = Router.extend(properties);
+    app.router = function(name, baseClassName, properties) {
+      var baseClass;
+      if (arguments.length === 2) {
+        properties = baseClassName;
+        baseClass = Router;
+      } else {
+        baseClass = app.Routers[baseClassName];
+      }
+
+      app.Routers[name] = baseClass.extend(properties);
     };
 
   });
