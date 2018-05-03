ff921c83695f7f3a66a1e756f91b1f936343266a
Donnie Tognazzini <dtognazzini@invoca.com>
2018-05-02 18:30:02 -0700

    TECH-2322 expose es6 modules: export DataTable.Config & ColumnType

--- src/namespace.js
+++ src/namespace.js
@@ -15,6 +15,8 @@ import ItemView from "./listing/item";
 import LocalDataTable from "./data_table/local_data_table";
 import ServerSideDataTable from "./data_table/server_side_data_table";
 import Row from "./data_table/row";
+import Config from "./data_table/config";
+import ColumnType from "./data_table/column_type";
 
 import "./legacy/register_base_plugin";
 import "./legacy/register_listing_plugin";
@@ -35,6 +37,8 @@ const Namespace = {
   Router,
 
   DataTable: {
+    Config,
+    ColumnType,
     Local: LocalDataTable,
     ServerSide: ServerSideDataTable,
     Row
