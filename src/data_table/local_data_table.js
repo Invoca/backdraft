b5b4c23d4b9c7936a8f59d9ca959eb050a92843c
Scott Chow <schow@invoca.com>
2018-07-11 11:19:43 -0700

    WEB-3741 update all management page urls with each pagination view: Refactor paging method for clarity.

--- src/data_table/local_data_table.js
+++ src/data_table/local_data_table.js
@@ -465,7 +465,7 @@ class LocalDataTable extends View {
   _setupPaginationHistory() {
     this.dataTable.on("page", () => {
       let page = this.dataTable.fnPagingInfo().iPage;
-      if (page !== this._parsePageNumberFromQueryString()) {
+      if (page !== this._parsePageNumberFromQueryString() - 1) {
         history.pushState({}, "pagination", this._createQueryStringWithPageNumber(page + 1));
       }
     });
@@ -481,7 +481,7 @@ class LocalDataTable extends View {
   }
 
   _goToPageFromQueryString() {
-    let pageNumber = this._parsePageNumberFromQueryString();
+    let pageNumber = this._parsePageNumberFromQueryString() - 1;
     if (pageNumber >= 0) {
       this.page(pageNumber);
     }
@@ -499,9 +499,9 @@ class LocalDataTable extends View {
 
   _parsePageNumberFromQueryString() {
     let parameters = this._urlParameters();
-    let page = parseInt(parameters.page) - 1;
+    let page = parseInt(parameters.page);
     if (isNaN(page)) {
-      return 0;
+      return 1;
     } else {
       return page;
     }
@@ -528,7 +528,7 @@ class LocalDataTable extends View {
   }
 
   _dataTableConfig() {
-    let displayStart = this._parsePageNumberFromQueryString() * this.paginateLength;
+    let displayStart = (this._parsePageNumberFromQueryString() - 1) * this.paginateLength;
     let recordTotal = displayStart + this.paginateLength;
     return {
       sDom: this.layout,
