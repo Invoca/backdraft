667bff56ab974c242b6f7309357d6dab10c754b8
Jack Chistyakov <jchistyakov@invoca.com>
2019-04-26 16:26:28 -0700

    WEB-4046: remove datatables old

--- src/data_table/datatables.js
+++ src/data_table/datatables.js
@@ -1,7 +1,11 @@
+import $ from "jquery";
 import "datatables"; // jQuery DataTables
+import "../../vendor/1.10_two_button"; // integration file for migrating from datatables 1.9 to 1.10
 
 import initializeBootstrap from "./datatables/configure";
 import initializeColReorderPlugin from "./datatables/col_reorder_with_resize";
+
+$.fn.dataTable.ext.legacy.ajax = true;
 
 initializeBootstrap();
 initializeColReorderPlugin();
