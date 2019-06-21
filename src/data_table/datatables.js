c8627ecd53f5021e24e0e6f24cbbff75c5548eba
Donnie Tognazzini <dtognazzini@invoca.com>
2019-06-21 12:42:53 -0700

    renaming files to match contents

--- src/data_table/datatables.js
+++
@@ -1,5 +0,0 @@
-import initializeBootstrap from "./datatables/initialize_bootstrap";
-import initializeColReorderPlugin from "./datatables/initialize_col_reorder_plugin";
-
-initializeBootstrap();
-initializeColReorderPlugin();
