d218b080acb1a965e77237201d541ad89971e2eb
Donnie Tognazzini <dtognazzini@invoca.com>
2019-06-21 12:38:34 -0700

    import jquery.dataTables where needed & remove dependency cycle

--- src/data_table/datatables.js
+++ src/data_table/datatables.js
@@ -1,5 +1,3 @@
-import "../../vendor/jquery.dataTables-1.9.4";
-
 import initializeBootstrap from "./datatables/configure";
 import initializeColReorderPlugin from "./datatables/col_reorder_with_resize";
 
